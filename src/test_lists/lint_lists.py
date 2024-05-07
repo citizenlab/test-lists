57b6cdd806167ae03d9c5b93dd23d0285530d001
Arturo Filastò <arturo@filasto.net>
2024-05-07 17:51:00 +0800

    Refactor lint-lists script

--- src/test_lists/lint_lists.py
+++ src/test_lists/lint_lists.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-
-from __future__ import print_function
-
-import argparse
 import datetime
 import os
 import re
@@ -10,10 +5,7 @@ import sys
 import csv
 from glob import glob
 
-try:
-    from urlparse import urlparse
-except:
-    from urllib.parse import urlparse
+from urllib.parse import urlparse
 
 VALID_URL = regex = re.compile(
     r"^(?:http)s?://"  # http:// or https://
@@ -26,8 +18,6 @@ VALID_URL = regex = re.compile(
 
 BAD_CHARS = ["\r", "\n", "\t", "\\"]
 
-CATEGORY_CODES = {}
-COUNTRY_CODES = {}
 
 NEW_CATEGORY_CODES = "00-LEGEND-new_category_codes.csv"
 LEGACY_CATEGORY_CODES = "00-LEGEND-category_codes.csv"
@@ -152,21 +142,101 @@ def check(url):
         return ERR_NOSLASH
 
 
-def lint_lists(lists_path, fix_duplicates=False, fix_slash=False):
+TEST_LISTS_HEADER = [
+    "url",
+    "category_code",
+    "category_description",
+    "date_added",
+    "source",
+    "notes",
+]
+
+
+class TestListProcessor:
+    def __init__(
+        self,
+        csv_path,
+        category_codes,
+        global_urls_bag,
+        fix_duplicates=False,
+        fix_slash=False,
+        fix_notes=False,
+    ):
+        self.csv_path = csv_path
+        self.global_urls_bag = global_urls_bag
+        self.fix_duplicates = fix_duplicates
+        self.fix_slash = fix_slash
+        self.fix_notes = fix_notes
+        self.errors = []
+        self.category_codes = category_codes
+
+        self.rows = [TEST_LISTS_HEADER]
+        self.urls_bag = set()
+        self.errors = []
+        self.idx = -1
+
+    def open(self):
+        return open(self.csv_path, "r", encoding="utf-8")
+
+    def process_row(self, idx, row):
+        if len(row) != 6:
+            self.errors.append(InvalidColumnNumber(self.csv_path, idx + 2))
+            return
+
+        url, cat_code, cat_desc, date_added, source, notes = row
+        err = check(url)
+        if err:
+            self.errors.append(InvalidURL(url, self.csv_path, idx + 2, details=err))
+            if err == ERR_NOSLASH and self.fix_slash:
+                row[0] = row[0] + "/"
+
+        if os.path.basename(self.csv_path) != "global.csv":
+            if url in self.global_urls_bag:
+                self.errors.append(
+                    DuplicateURLWithGlobalList(url, self.csv_path, idx + 2)
+                )
+                if self.fix_duplicates:
+                    return
+
+        try:
+            cat_description = self.category_codes[cat_code]
+        except KeyError:
+            self.errors.append(InvalidCategoryCode(cat_code, self.csv_path, idx + 2))
+        if cat_description != cat_desc:
+            self.errors.append(InvalidCategoryDesc(cat_desc, self.csv_path, idx + 2))
+        if url in self.urls_bag:
+            if not self.fix_duplicates:
+                self.errors.append(DuplicateURL(url, self.csv_path, idx + 2))
+            return
+
+        if not is_valid_date(date_added):
+            self.errors.append(InvalidDate(date_added, self.csv_path, idx + 2))
+        if any([c in notes for c in BAD_CHARS]):
+            self.errors.append(InvalidNotes(notes, self.csv_path, idx + 2))
+        if any([c in source for c in BAD_CHARS]):
+            self.errors.append(InvalidSource(source, self.csv_path, idx + 2))
+        self.urls_bag.add(url)
+
+        self.rows.append(row)
+
+    def write_fixed(self):
+        with open(self.csv_path + ".fixed", "w") as out_file:
+            csv_writer = csv.writer(
+                out_file, quoting=csv.QUOTE_MINIMAL, lineterminator="\n"
+            )
+            csv_writer.writerows(self.rows)
+        os.rename(self.csv_path + ".fixed", self.csv_path)
+
+
+def lint_lists(lists_path, fix_duplicates=False, fix_slash=False, fix_notes=False):
     all_errors = []
     total_urls = 0
     total_countries = 0
-    CATEGORY_CODES = load_categories(
+
+    category_codes = load_categories(
         os.path.join(lists_path, NEW_CATEGORY_CODES), get_new_description_code
     )
-    header = [
-        "url",
-        "category_code",
-        "category_description",
-        "date_added",
-        "source",
-        "notes",
-    ]
+
     # preload the global list to check against looking for dupes
     global_urls_bag = load_global_list(os.path.join(lists_path, "global.csv"))
     for csv_path in glob(os.path.join(lists_path, "*")):
@@ -174,83 +244,33 @@ def lint_lists(lists_path, fix_duplicates=False, fix_slash=False):
             continue
         if not csv_path.endswith(".csv"):
             continue
-        with open(csv_path, "r", encoding="utf-8") as in_file:
+        processor = TestListProcessor(
+            csv_path,
+            category_codes=category_codes,
+            global_urls_bag=global_urls_bag,
+            fix_duplicates=fix_duplicates,
+            fix_notes=fix_notes,
+            fix_slash=fix_slash,
+        )
+        with processor.open() as in_file:
             reader = csv.reader(in_file, delimiter=",")
             first_line = next(reader)
-            if first_line != header:
-                errors.append(InvalidHeader(csv_path, 0))
-            urls_bag = set()
-            errors = []
-            rows = []
-            duplicates = 0
-            without_slash = 0
-            idx = -1
-            for idx, row in enumerate(reader):
-                if len(row) != 6:
-                    errors.append(InvalidColumnNumber(csv_path, idx + 2))
-                    continue
-                url, cat_code, cat_desc, date_added, source, notes = row
-                err = check(url)
-                if err:
-                    errors.append(InvalidURL(url, csv_path, idx + 2, details=err))
-                    if err == ERR_NOSLASH:
-                        without_slash += 1
-                        row[0] = row[0] + "/"
-                if os.path.basename(csv_path) != "global.csv":
-                    if url in global_urls_bag:
-                        errors.append(
-                            DuplicateURLWithGlobalList(url, csv_path, idx + 2)
-                        )
-                        if fix_duplicates:
-                            duplicates += 1
-                            continue
+            if first_line != TEST_LISTS_HEADER:
+                processor.errors.append(InvalidHeader(csv_path, 0))
 
-                try:
-                    cat_description = CATEGORY_CODES[cat_code]
-                except KeyError:
-                    errors.append(InvalidCategoryCode(cat_code, csv_path, idx + 2))
-                if cat_description != cat_desc:
-                    errors.append(InvalidCategoryDesc(cat_desc, csv_path, idx + 2))
-                if url in urls_bag:
-                    if not fix_duplicates:
-                        errors.append(DuplicateURL(url, csv_path, idx + 2))
-                    duplicates += 1
-                    continue
-                if not is_valid_date(date_added):
-                    errors.append(InvalidDate(date_added, csv_path, idx + 2))
-                if any([c in notes for c in BAD_CHARS]):
-                    errors.append(InvalidNotes(notes, csv_path, idx + 2))
-                if any([c in source for c in BAD_CHARS]):
-                    errors.append(InvalidSource(source, csv_path, idx + 2))
-                urls_bag.add(url)
-                rows.append(row)
-            print("* {}".format(csv_path))
-            print("  {} URLs".format(idx + 1))
-            print("  {} Errors".format(len(errors)))
-            all_errors += errors
+            for idx, row in enumerate(reader):
+                processor.process_row(idx, row)
+
+            if fix_slash or fix_duplicates:
+                processor.write_fixed()
+
+            print(f"* {processor.csv_path}")
+            print(f"  {idx+1} URLs")
+            print(f"  {len(processor.errors)} Errors")
+
+            all_errors += processor.errors
             total_urls += idx + 1
             total_countries += 1
-
-        if fix_slash and without_slash > 0:
-            print("Fixing slash in %s" % csv_path)
-            rows.insert(0, header)
-            with open(csv_path + ".fixed", "w") as out_file:
-                csv_writer = csv.writer(
-                    out_file, quoting=csv.QUOTE_MINIMAL, lineterminator="\n"
-                )
-                csv_writer.writerows(rows)
-            os.rename(csv_path + ".fixed", csv_path)
-
-        if fix_duplicates and duplicates > 0:
-            rows.sort(key=lambda x: x[0].split("//")[1])
-            rows.insert(0, header)
-            with open(csv_path + ".fixed", "w") as out_file:
-                csv_writer = csv.writer(
-                    out_file, quoting=csv.QUOTE_MINIMAL, lineterminator="\n"
-                )
-                csv_writer.writerows(rows)
-            print("Sorting %s - Found %d duplicates" % (csv_path, duplicates))
-            os.rename(csv_path + ".fixed", csv_path)
 
     print("----------")
     print("Analyzed {} URLs in {} countries".format(total_urls, total_countries))
